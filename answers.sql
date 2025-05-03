ea8bcaa238abe6badaaf0697323c28ef241dc949
lisanzatabby <lisanzatabitha@gmail.com>
2025-05-03 23:03:56 +0300

    Week 7 Assignment Corrections

--- answers.sql
+++ answers.sql
@@ -11,71 +11,39 @@ CREATE TABLE ProductDetail (
   Products VARCHAR(100)
 );
 -- Inserting Data into Table 1
-INSERT INTO ProductDetail (OrderID, CustomerName, Products) VALUES
-(101, 'John Doe', 'Laptop, Mouse'),
-(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
 (103, 'Emily Clark', 'Phone');
 
--- Converting to 1NF  using the Recusive CTE method (split_products)
-
- WITH RECURSIVE split_products AS (
-  SELECT 
-    OrderID,
-    CustomerName,
-    TRIM(SUBSTRING_INDEX(Products, ',', 1)) AS Product,
-    SUBSTRING(Products, LENGTH(SUBSTRING_INDEX(Products, ',', 1)) + 2) AS rest
-  FROM ProductDetail
-
-  UNION ALL
-
-  SELECT 
-    OrderID,
-    CustomerName,
-    TRIM(SUBSTRING_INDEX(rest, ',', 1)) AS Product,
-    SUBSTRING(rest, LENGTH(SUBSTRING_INDEX(rest, ',', 1)) + 2)
-  FROM split_products
-  WHERE rest != ''
-)
-SELECT OrderID, CustomerName, Product
-FROM split_products
-ORDER BY OrderID;
-
--- Quetsion 2 Acheiving 2NF on the Order details table
-Use DB1;
--- create ordetailes table
-CREATE TABLE OrderDetails (
-    OrderID INT,
-    CustomerName VARCHAR(100),
-    Product VARCHAR(100),
-    Quantity INT
-);
--- inserting data into orderdetails
-INSERT INTO OrderDetails VALUES
-(101, 'John Doe', 'Laptop', 2),
-(101, 'John Doe', 'Mouse', 1),
-(102, 'Jane Smith', 'Tablet', 3),
-(102, 'Jane Smith', 'Keyboard', 1),
-(102, 'Jane Smith', 'Mouse', 2),
-(103, 'Emily Clark', 'Phone', 1);
-
--- Removing partial dependencies customerName by creating customer table and importing relevant data from the orderdetails table
-CREATE TABLE Customers (
+-- Question 2 Acheiving 2NF on the Order details table
+CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-INSERT INTO Customers
-SELECT DISTINCT OrderID, CustomerName
-FROM OrderDetails;
 
--- removing orders from the OrderDetails table into the Orderitems table and importing relevant data from the OrderDetails table 
-CREATE TABLE OrderItems (
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
-    FOREIGN KEY (OrderID) REFERENCES Customers(OrderID)
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
-
-INSERT INTO OrderItems
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
